0367d09b200f67f7d847aafda4926b6f49f9a4af
Taerin Lee <taerin.lee@inhousesoft.com>
2024-09-17 23:17:45 +0900

    #12 미사용 코드 제거

--- src/app/api/recruitment/route.js
+++ src/app/api/recruitment/route.js
@@ -3,7 +3,7 @@ import recruitmentList from "@/__mocks__/recruitment_list.json";
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
-  const category = searchParams.get("category") || "01";
+  const category = searchParams.get("category");
 
   const filteredPosts = recruitmentList.filter(
     (post) => post.category === category
