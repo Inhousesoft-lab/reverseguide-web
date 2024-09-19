80ab3a1efa5be51cb52693fe33e4699b6b858eda
Taerin Lee <taerin.lee@inhousesoft.com>
2024-09-19 17:13:06 +0900

    #18 뉴스 카드 위젯 정리

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -22,8 +22,16 @@ export default function NewsList() {
           </h2>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {googleSheetRows.map((item) => {
-            return <NewsCard key={item[0]} item={item} />;
+          {googleSheetRows.map((el) => {
+            const item = {
+              id: el[0],
+              title: el[1],
+              viewDate: el[2],
+              image01: el[3],
+              image02: el[4],
+              link: el[5],
+            };
+            return <NewsCard key={item.id} item={item} />;
           })}
         </div>
       </div>
--- src/widgets/NewsCard.js
+++ src/widgets/NewsCard.js
@@ -1,24 +1,25 @@
 import Image from "next/image";
 
 export default function NewsCard({ item }) {
+  const { title, viewDate, image01, image02, link } = item;
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href={item[5]} target="_blank">
+      <a href={link} target="_blank">
         <div className="relative w-full h-48">
           <Image
             className="object-cover"
-            src={item[3]}
-            alt={item[1]}
+            src={image01}
+            alt={title}
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
         <div className="p-5">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {item[1]}
+            {title}
           </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {item[2]}
+            {viewDate}
           </p>
         </div>
       </a>
