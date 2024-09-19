2fec2b15cfc798f4a60706e4932384e021374775
Taerin Lee <taerin.lee@inhousesoft.com>
2024-09-19 18:36:41 +0900

    #9 뉴스 리팩토링

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,17 +1,32 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import useGoogleSheet from "@/hooks/useGooglesheet";
 import { GOOGLE_SHEET_ID, GID_LIST } from "@/constants/google-sheet";
 import NewsCard from "@/widgets/NewsCard";
 
 export default function NewsList() {
+  const [rowData, setRowData] = useState([]);
   const { googleSheetRows, isLoading, error } = useGoogleSheet(
     GOOGLE_SHEET_ID,
     GID_LIST.NEWS
   );
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  useEffect(() => {
+    if (googleSheetRows) {
+      const formattedRows = googleSheetRows.map((row) => {
+        return {
+          id: row[0],
+          title: row[1],
+          viewDate: row[2],
+          image01: row[3],
+          image02: row[4],
+          link: row[5],
+        };
+      });
+      setRowData(formattedRows);
+    }
+  }, [googleSheetRows]);
 
   return (
     <div className="bg-white py-24 sm:py-32">
@@ -22,18 +37,25 @@ export default function NewsList() {
           </h2>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {googleSheetRows.map((el) => {
-            const item = {
-              id: el[0],
-              title: el[1],
-              viewDate: el[2],
-              image01: el[3],
-              image02: el[4],
-              link: el[5],
-            };
-            return <NewsCard key={item.id} item={item} />;
-          })}
+          {isLoading || error
+            ? [...Array(6)].map((_, index) => <SkeletonNewsCard key={index} />)
+            : rowData.map((item) => {
+                return <NewsCard key={item.id} item={item} />;
+              })}
         </div>
+      </div>
+    </div>
+  );
+}
+
+function SkeletonNewsCard() {
+  return (
+    <div className="w-full bg-white rounded-xl shadow-md overflow-hidden animate-pulse">
+      <div className="aspect-w-16 aspect-h-9 bg-gray-300"></div>
+      <div className="p-4">
+        <div className="h-20 bg-gray-300 rounded w-3/4 mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-full mb-2"></div>
+        <div className="h-3 bg-gray-300 rounded w-1/4 mt-4"></div>
       </div>
     </div>
   );
