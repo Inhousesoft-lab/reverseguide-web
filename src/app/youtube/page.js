fac5c4344284959143cbc397a4e50b51a76a7311
Taerin Lee <taerin.lee@inhousesoft.com>
2024-09-19 18:30:52 +0900

    #8 youtube page 리팩토링

--- src/app/youtube/page.js
+++ src/app/youtube/page.js
@@ -1,47 +1,64 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import useGoogleSheet from "@/hooks/useGooglesheet";
 import { GOOGLE_SHEET_ID, GID_LIST } from "@/constants/google-sheet";
 import { formatRelativeTime } from "@/utils/dateToString";
 import YoutubeCard from "@/widgets/YoutubeCard";
-
-function extractVideoId(url) {
-  const regExp =
-    /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-  const match = url.match(regExp);
-  return match && match[2].length === 11 ? match[2] : null;
-}
+import { extractVideoId } from "@/utils/youtubeConverter";
 
 export default function YoutubeList() {
+  const [rowData, setRowData] = useState([]);
   const { googleSheetRows, isLoading, error } = useGoogleSheet(
     GOOGLE_SHEET_ID,
     GID_LIST.YOUTUBE
   );
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  useEffect(() => {
+    if (googleSheetRows) {
+      const formattedVideos = googleSheetRows.map((row) => {
+        return {
+          id: row[0],
+          title: row[1],
+          description: row[2],
+          url: row[3],
+          videoId: extractVideoId(row[3]),
+          uploadDate: formatRelativeTime(row[4]),
+        };
+      });
+      setRowData(formattedVideos);
+    }
+  }, [googleSheetRows]);
 
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            YOUTUBE
-          </h2>
+    <div className="bg-white py-12 sm:py-24">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900 mb-8">
+          YouTube Videos
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {isLoading || error
+            ? [...Array(6)].map((_, index) => (
+                <SkeletonYouTubeCard key={index} />
+              ))
+            : rowData.map((item) => {
+                return <YoutubeCard key={item.id} item={item} />;
+              })}
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {googleSheetRows.map((el) => {
-            const item = {
-              id: el[0],
-              title: el[1],
-              description: el[2],
-              youtubeUrl: el[3],
-              videoId: extractVideoId(el[3]),
-              uploadDate: formatRelativeTime(el[4]),
-            };
-            return <YoutubeCard key={item.id} item={item} />;
-          })}
-        </div>
+      </div>
+    </div>
+  );
+}
+
+function SkeletonYouTubeCard() {
+  return (
+    <div className="w-full bg-white rounded-xl shadow-md overflow-hidden animate-pulse">
+      <div className="aspect-w-16 aspect-h-9 bg-gray-300"></div>
+      <div className="p-4">
+        <div className="h-20 bg-gray-300 rounded w-3/4 mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-full mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+        <div className="h-3 bg-gray-300 rounded w-1/4 mt-4"></div>
       </div>
     </div>
   );
--- src/widgets/YoutubeCard.js
+++ src/widgets/YoutubeCard.js
@@ -1,8 +1,8 @@
 export default function YoutubeCard({ item }) {
   const { title, description, youtubeUrl, uploadDate, videoId } = item;
   return (
-    <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
-      <div className="flex-shrink-0">
+    <div className="bg-white rounded-xl shadow-md overflow-hidden">
+      <div className="aspect-w-16 aspect-h-9">
         <iframe
           className="h-48 w-full object-cover border-none"
           src={`https://www.youtube.com/embed/${videoId}`}
@@ -11,19 +11,10 @@ export default function YoutubeCard({ item }) {
           allowFullScreen
         ></iframe>
       </div>
-      <div className="flex flex-1 flex-col justify-between bg-white p-6">
-        <div className="flex-1">
-          <a href={youtubeUrl} target="_blank" className="mt-2 block">
-            <p className="text-xl font-semibold text-gray-900">{title}</p>
-            <p className="mt-3 text-base text-gray-500">{description}</p>
-          </a>
-        </div>
-        <div className="mt-6 flex items-center">
-          <div className="flex-shrink-0">
-            <span className="sr-only">Upload date</span>
-            <p className="text-sm text-gray-500">{uploadDate}</p>
-          </div>
-        </div>
+      <div className="p-4">
+        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+        <p className="text-gray-600 mb-2">{description}</p>
+        <p className="text-sm text-gray-500">{uploadDate}</p>
       </div>
     </div>
   );
