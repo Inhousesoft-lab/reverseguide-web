85b8b54c5c4f920c238f7e98e25b6daaa73ca074
Taerin Lee <taerin.lee@inhousesoft.com>
2024-09-19 18:41:28 +0900

    #18 뉴스 ui update

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -29,14 +29,10 @@ export default function NewsList() {
   }, [googleSheetRows]);
 
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            News
-          </h2>
-        </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+    <div className="bg-white py-12 sm:py-24">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900 mb-8">News</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {isLoading || error
             ? [...Array(6)].map((_, index) => <SkeletonNewsCard key={index} />)
             : rowData.map((item) => {
